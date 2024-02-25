ba7e6e26e4e04bff564c1cfacaba496a2c90f441
Serhii Smilianets <Serhii_Smilianets@epam.com>
2024-02-25 12:16:49 +0100

    Module 3: initial commit

--- src/components/GenreList.js
+++ src/components/GenreList.js
@@ -1,6 +1,6 @@
 
 import React, { useState } from "react";
-import '../GenreList.css';
+import '../components-styles/GenreList.scss';
 
 const GenreBtn = ({genre, isSelected, onGenreSelection}) => {
     return (
