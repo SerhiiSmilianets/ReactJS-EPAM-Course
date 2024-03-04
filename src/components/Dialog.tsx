cec4d01792e485baa6147f559f6a295e59c5bb2a
Serhii Smilianets <Serhii_Smilianets@epam.com>
2024-03-04 13:21:22 +0100

    Module 4: Created MovieForm and Dialog, added styles and stories

--- src/components/Dialog.tsx
+++ src/components/Dialog.tsx
@@ -1,32 +1,3 @@
-// import '../components-styles/Dialog.scss';
-
-// const Dialog = ({title, onClose, children}) => {
-//     const handleOnClose = () => {
-//         if (typeof onClose === "function") {
-//             onClose()
-//         }
-//     }
-//     if (!children) {
-//         return (<h1>No Child Element Being Provided</h1>)
-//     }
-//     return (
-//         <>
-//             <div className="dialog__overlay" onClick={handleOnClose}></div>
-//             <div className="dialog__container">
-//                 <div className="dialog__header">
-//                     <h2>{title}</h2>
-//                     <button onClick={handleOnClose}>&times;</button>
-//                 </div>
-//                 <div className="dialog__content">
-//                     {children}
-//                 </div>
-//             </div>
-//         </>
-//     )
-// }
-
-// export default Dialog
-
 import React, { ReactNode, MouseEvent } from 'react';
 import '../components-styles/Dialog.scss';
 
--- src/components/MovieForm.tsx
+++ src/components/MovieForm.tsx
@@ -1,11 +1,29 @@
+import React, { FormEvent } from 'react';
 import '../components-styles/MovieForm.scss';
+import {GenreList} from '../constants'
 
-const MovieForm = ({movieData, onSubmit, isDeleteForm}) => {
+interface MovieData {
+  title: string;
+  release_date: string;
+  poster_path: string;
+  vote_average: number;
+  genres: string[];
+  runtime: number;
+  overview: string;
+}
+
+interface MovieFormProps {
+  movieData?: MovieData;
+  onSubmit: (event: FormEvent<HTMLFormElement>) => void;
+  isDeleteForm: boolean;
+}
+
+const MovieForm: React.FC<MovieFormProps> = ({movieData, onSubmit, isDeleteForm}) => {
     if (isDeleteForm) {
         return (
             <div className='movie-form__container'>
                 <p>Are you sure you want delete this movie?</p>
-                <form action={onSubmit}>
+                <form onSubmit={onSubmit}>
                     <div className='movie-form__row form-buttons__container'>
                         <button className='btn submit' type='submit'>Submit</button>
                     </div>
@@ -16,51 +34,53 @@ const MovieForm = ({movieData, onSubmit, isDeleteForm}) => {
 
     return (
         <div className='movie-form__container'>
-            <form action={onSubmit}>
+            <form onSubmit={onSubmit}>
                 <div className='movie-form__row'>
                     <div className='movie-form__field-column left'>
                         <label htmlFor="movie_title">Title</label>
-                        <input id="movie_title" name="movie_title" type="text" placeholder="Movie title" defaultValue={movieData && movieData.title ? movieData.title : ""}/>
+                        <input id="movie_title" name="movie_title" type="text" placeholder="Movie title" defaultValue={movieData?.title || ""} />
                     </div>
 
                     <div className='movie-form__field-column right'>
                         <label htmlFor="movie_release_date">Release Date</label>
-                        <input id="movie_release_date" name="movie_release_date" type="text" placeholder="Select date" defaultValue={movieData && movieData.release_date ? movieData.release_date  : ""}/>
+                        <input id="movie_release_date" name="movie_release_date" type="text" placeholder="Select date" defaultValue={movieData?.release_date || ""} />
                     </div>
                 </div>
 
                 <div className='movie-form__row'>
                     <div className='movie-form__field-column left'>
                         <label htmlFor="movie_poster_url">Movie URL</label>
-                        <input id="movie_poster_url" name="movie_poster_url" type="text" placeholder="https://" defaultValue={movieData && movieData.poster_path ? movieData.poster_path  : ""}/>
+                        <input id="movie_poster_url" name="movie_poster_url" type="text" placeholder="https://" defaultValue={movieData?.poster_path || ""} />
                     </div>
 
                     <div className='movie-form__field-column right'>
                         <label htmlFor="movie_rating">Rating</label>
-                        <input id="movie_rating" name="movie_rating" type="number" placeholder="7.8" defaultValue={movieData && movieData.vote_average  ? movieData.vote_average  : ""}/>
+                        <input id="movie_rating" name="movie_rating" type="number" placeholder="7.8" defaultValue={movieData?.vote_average || ""} />
                     </div>
                 </div>
 
                 <div className='movie-form__row'>
                     <div className='movie-form__field-column left'>
                         <label htmlFor="movie_genre">Genre</label>
-                        <select name="movie_genre" id="movie_genre">
+                        <select name="movie_genre" id="movie_genre" defaultValue={movieData?.genres || ""}>
                             <option value="">Select Genre</option>
+                            {GenreList.map((item) =>
+                                <option key={item} value={item}>{item}</option>
+                            )}
                         </select>
                     </div>
                     <div className='movie-form__field-column right'>
                         <label htmlFor="movie_runtime">Runtime</label>
-                        <input id="movie_runtime" name="movie_runtime" type="number" placeholder="minutes" defaultValue={movieData && movieData.runtime ? movieData.runtime : ""}/>
+                        <input id="movie_runtime" name="movie_runtime" type="number" placeholder="minutes" defaultValue={movieData?.runtime || ""} />
                     </div>
                 </div>
 
                 <div className='movie-form__row'>
                     <div className='movie-form__field-column text-area__container'>
                         <label htmlFor="movie_overview">Overview</label>
-                        <textarea id="movie_overview" name="movie_overview" type="textarea" placeholder="Movie description" defaultValue={movieData && movieData.overview ? movieData.overview  : ""}/>
+                        <textarea id="movie_overview" name="movie_overview" placeholder="Movie description" defaultValue={movieData?.overview || ""} />
                     </div>
                 </div>
-                
 
                 <div className='movie-form__row form-buttons__container'>
                     <button className='btn reset' type='reset'>Reset</button>
@@ -72,4 +92,3 @@ const MovieForm = ({movieData, onSubmit, isDeleteForm}) => {
 }
 
 export default MovieForm
-
